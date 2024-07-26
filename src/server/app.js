c4152ca5ee5160d82811ab96b18014e757235a48
Nirav <niravpatelp129@gmail.com>
2024-07-26 17:17:40 -0400

    * fix(server): removed unnecessary options

--- src/server/app.js
+++ src/server/app.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Could not connect to MongoDB', err));
 
